6135a6a3f7ede831f3c0430151fe8707e3da5a2b
alanbbarros <alanbritobarros@gmail.com>
2022-02-15 11:35:52 -0300

    completed tictactoe game

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -4,6 +4,7 @@ const JOGADOR_O = 'O'
 let turno = true;
 const winCondition = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 let winner = false;
+let jogadas = 0
 
 
 function checarVencedor(jogador){
@@ -14,9 +15,31 @@ function checarVencedor(jogador){
     })
 }
 
+function encerraJogo(ganhador, winner){
+    if(winner){
+        document.getElementById('winner').innerText = `O JOGADOR ${ganhador} VENCEU!`
+    }
+    else{
+        document.getElementById('winner').innerText = 'Empate'
+    }
 
-function jogar(id){
+    let botaoJogarNovamente = document.createElement('button')
+    let textoBotao = document.createTextNode('Jogar novamente');
+    botaoJogarNovamente.appendChild(textoBotao)
+    let section = document.getElementsByTagName('section')[0]
+    section.appendChild(botaoJogarNovamente)
 
+    botaoJogarNovamente.addEventListener('click', function(){
+        location.reload();
+    })
+}
+
+
+function jogar(id, jogadas){
+
+    if(jogadas == 9){
+        encerraJogo(null, winner)
+    }
     const chosenSquare = document.getElementById(id)
     const isEmpty = chosenSquare.innerText ? false : true
     
@@ -27,7 +50,8 @@ function jogar(id){
         let hasWinner = checarVencedor(JOGADOR_X)
         turno = !turno;
         if(hasWinner){
-            console.log('ganhou o X');
+            winner = true;
+            encerraJogo(JOGADOR_X, winner);
         }
     }
     else if(!turno && isEmpty){ // turno false = vez do O
@@ -36,14 +60,17 @@ function jogar(id){
         let hasWinner = checarVencedor(JOGADOR_O)
         turno = !turno;
         if(hasWinner){
-            console.log('ganhou o O');
+            winner = true;
+            encerraJogo(JOGADOR_O, winner);
         }
     }
 
 }
 
+
 document.addEventListener('mousedown', function(e){
     if(e.target.matches('.square')){
-        jogar(e.target.id);
+        jogadas++;
+        jogar(e.target.id, jogadas);
     }
 })
